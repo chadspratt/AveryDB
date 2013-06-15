43ef081974dada6cf4a4eaef48d0426248377605
Chad Spratt <chad@dogtato.net>
2013-06-15 19:55:12 -0200

    Added Licensing (Apache 2.0 (hopefully I did it correctly))

--- record.py
+++ record.py
@@ -1,5 +1,20 @@
 # -*- coding: utf-8 -*-
-# maybe unneeded?
+##
+#   Copyright 2013 Chad Spratt
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+##
+
+# Is this object needed?
 class Record(dict):
     def __init__(self):
         dict.__init__()
@@ -9,3 +24,6 @@ class Record(dict):
     
     def __setitem__(self, key, value):
         dict.__setitem__(key, value)
+        
+        Copyright [yyyy] [name of copyright owner]
+
